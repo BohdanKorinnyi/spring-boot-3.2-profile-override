7f12fba29f868956deb489d3b93333bc122ca29f
Bohdan Korinnyi <noreply@gmail.com>
2023-11-24 14:43:14 +0200

    Example of working ConfigurationProperties binding on Spring Boot 3.2.0

--- src/main/java/com/example/profile/props/ApplicationTokenProperties.java
+++ src/main/java/com/example/profile/props/ApplicationTokenProperties.java
@@ -1,19 +1,17 @@
 package com.example.profile.props;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 @ConfigurationProperties("api.token")
 public class ApplicationTokenProperties {
-    private final Integer refreshInterval;
-
-    // doesn't work on Spring Boot 3.2.0
-    @ConstructorBinding
-    public ApplicationTokenProperties(Integer refreshInterval) {
-        this.refreshInterval = refreshInterval;
-    }
+    private Integer refreshInterval;
 
     public Integer getRefreshInterval() {
         return refreshInterval;
+    }
+
+    // works on Spring Boot 3.2.0
+    public void setRefreshInterval(Integer refreshInterval) {
+        this.refreshInterval = refreshInterval;
     }
 }
